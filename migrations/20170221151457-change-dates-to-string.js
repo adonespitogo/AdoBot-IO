7cec1d3a11a0e4d0f23167c660e047c6985ede48
Adones Pitogo <pitogo.adones@gmail.com>
2017-02-21 23:20:07 +0800

    change dates to str

--- migrations/20170221151457-change-dates-to-string.js
+++ migrations/20170221151457-change-dates-to-string.js
@@ -2,7 +2,7 @@
 var q = require('q')
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
+  up: function(queryInterface, Sequelize) {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -10,12 +10,17 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-      var q1 = queryInterface.changeColumn('messages', 'date', { type: Sequelize.STRING(50) });
-      var q2 = queryInterface.changeColumn('call_logs', 'date', { type: Sequelize.STRING(50) });
-      return q.all([q1, q2])
+    var q1 = queryInterface.changeColumn('messages', 'date', {
+      type: Sequelize.STRING(50),
+    });
+    var q2 = queryInterface.changeColumn('call_logs', 'date', {
+      type: Sequelize.STRING(50),
+      unique: true
+    });
+    return q.all([q1, q2])
   },
 
-  down: function (queryInterface, Sequelize) {
+  down: function(queryInterface, Sequelize) {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -23,8 +28,13 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-      var q1 = queryInterface.changeColumn('messages', 'date', { type: Sequelize.DATE });
-      var q2 = queryInterface.changeColumn('call_logs', 'date', { type: Sequelize.DATE });
-      return q.all([q1, q2])
+    var q1 = queryInterface.changeColumn('messages', 'date', {
+      type: Sequelize.DATE
+    });
+    var q2 = queryInterface.changeColumn('call_logs', 'date', {
+      type: Sequelize.DATE,
+      unique: true
+    });
+    return q.all([q1, q2])
   }
 };
