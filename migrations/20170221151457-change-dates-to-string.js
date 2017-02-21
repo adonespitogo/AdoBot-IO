e1cb9353ea39402240a298f5482ec4c94f5d53d3
Adones Pitogo <pitogo.adones@gmail.com>
2017-02-21 23:17:48 +0800

    change dates to str

---
+++ migrations/20170221151457-change-dates-to-string.js
@@ -0,0 +1,30 @@
+'use strict';
+var q = require('q')
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+      var q1 = queryInterface.changeColumn('messages', 'date', { type: Sequelize.STRING(50) });
+      var q2 = queryInterface.changeColumn('call_logs', 'date', { type: Sequelize.STRING(50) });
+      return q.all([q1, q2])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+      var q1 = queryInterface.changeColumn('messages', 'date', { type: Sequelize.DATE });
+      var q2 = queryInterface.changeColumn('call_logs', 'date', { type: Sequelize.DATE });
+      return q.all([q1, q2])
+  }
+};
