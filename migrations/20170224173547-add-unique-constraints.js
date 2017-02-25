c93fd5a4d7d66c8f9f834b1b6461a31874b41f1b
Adones Pitogo <pitogo.adones@gmail.com>
2017-02-25 21:14:20 +0800

    fix call logs unique

--- migrations/20170224173547-add-unique-constraints.js
+++ migrations/20170224173547-add-unique-constraints.js
@@ -14,7 +14,7 @@ module.exports = {
     */
 
     var q1 = sequelize.query('ALTER TABLE messages ADD CONSTRAINT u_msg UNIQUE (uid,message_id);')
-    var q2 = sequelize.query('ALTER TABLE call_logs ADD CONSTRAINT u_call_log UNIQUE (uid,call_id);')
+    var q2 = sequelize.query('ALTER TABLE call_logs ADD CONSTRAINT u_call_log UNIQUE (uid,date,type);')
     var q3 = sequelize.query('ALTER TABLE contacts ADD CONSTRAINT u_contact UNIQUE (uid,contact_id);')
 
     return q.all([q1, q2, q3])
